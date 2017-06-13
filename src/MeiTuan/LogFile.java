e76e0dee9a8b66dac1812a0d0175d01af532047a
luoyu <z_y_ang@163.com>
2017-06-13 23:05:02 +0800

    add ignore

--- src/MeiTuan/LogFile.java
+++ src/MeiTuan/LogFile.java
@@ -2,44 +2,101 @@ package MeiTuan;
 
 import java.io.File;
 import java.io.FileNotFoundException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 
 /**
  * Computer user xd
  * Created by 张洋 on 2017/6/12.
  */
 public class LogFile {
+    public static class BadLog{
+        public int num = 0;
+        public ArrayList<Integer> log = new ArrayList<>();
+        public void put(int data){
+            log.add(data);
+            num++;
+        }
+        public boolean exist(int smallest){
+            boolean result = false;
+            Iterator<Integer> it = log.iterator();
+            int index = 0;
+            while (it.hasNext()){
+                if(it.next() > smallest){
+                    result = true;
+                    remove(index);
+                }
+                index++;
+            }
+            return result;
+        }
+
+        private void remove(int index){
+            num--;
+            log.set(index,-2);
+        }
+
+        public int size(){
+            return num;
+        }
+    }
     public static void main(String[] args) throws FileNotFoundException {
         Scanner sc = new Scanner(new File("input2"));
         while (sc.hasNextInt()){
             int errorLine = -1;
             int num = 0;
-            int badLogNum =0;
-            Object obj = new Object();
-            Map<String,Object> map = new HashMap<>();
+            Map<String,Integer> buyMap = new HashMap<>();
+            Map<String,Integer> useMap = new HashMap<>();
+            BadLog unLog = new BadLog();
             num = sc.nextInt();
             sc.nextLine();
             for (int i = 0; i < num; i++) {
                 String line = sc.nextLine();
                 if(line.charAt(0) == '?'){
-                    badLogNum++;
-                }else if(line.charAt(0) == 'I'){
-                    if(map.get(line.substring(2)) != null){
-                        errorLine = i+1;
-                        break;
-                    }else {
-                        map.put(line.substring(2),obj);
-                    }
-                }else if(line.charAt(0) == 'O'){
-                    if(map.get(line.substring(2)) != null){
-                        map.remove(line.substring(2));
-                    }else if(badLogNum >0){
-                        badLogNum--;
-                    }else {
-                        errorLine = i+1;
-                        break;
+                    unLog.put(i);
+                }else {
+                    String substring = line.substring(2);
+                    if(line.charAt(0) == 'I'){
+                        Integer inputLine = buyMap.get(substring);
+                        if( inputLine!= null){
+                            boolean exist = false;
+                            if(unLog.size() >0){
+    //                            存在unlog的情况
+                                if(unLog.exist(inputLine)){
+                                    buyMap.put(substring,i);
+                                    exist = true;
+                                    break;
+                                }
+                            }
+                            if(!exist){
+                                errorLine = i+1;
+                                break;
+                            }
+                        }else {
+                            buyMap.put(substring,i);
+                        }
+                    }else if(line.charAt(0) == 'O'){
+                        if(buyMap.get(substring) != null){
+                            buyMap.remove(substring);
+                            useMap.put(substring,i);
+    //                        判断未知log
+                        }else {
+                            boolean exist = false;
+                            Integer lastUse = useMap.get(substring);
+                            if(lastUse != null){
+                                if(unLog.exist(lastUse)){
+                                    exist = true;
+                                    break;
+                                }
+                            }else if (unLog.size() > 0){
+                                exist = true;
+                                unLog.remove(0);
+                            }
+
+                            if(!exist){
+                                errorLine = i+1;
+                                break;
+                            }
+                        }
                     }
                 }
             }
--- src/MeiTuan/WriteTest.java
+++ src/MeiTuan/WriteTest.java
@@ -9,15 +9,17 @@ import java.io.IOException;
  */
 public class WriteTest {
     public static void main(String[] args) throws IOException {
-        FileOutputStream wr = new FileOutputStream("input.txt");
-        int couple = 4;
-        int data = (int) Math.pow(2,couple);
-        System.out.println(data);
-        wr.write((data+"\n").getBytes());
-        for (int i = 0; i < data; i++) {
-            if(i < (data>>1)) wr.write("-1000000 ".getBytes());
-            else wr.write("1000000 ".getBytes());
+        FileOutputStream wr = new FileOutputStream("input2");
+        int dataNum = (int) (5*Math.pow(10,5));
+//        int data = (int) Math.pow(2,couple);
+        System.out.println(dataNum);
+        wr.write((dataNum+"\n").getBytes());
+        wr.write(("I 1\n").getBytes());
+        for (int i = 1; i < dataNum-2; i++) {
+            wr.write("?\n".getBytes());
         }
+        wr.write("I 1\n".getBytes());
+        wr.write("I 1\n".getBytes());
         wr.flush();
         wr.close();
     }
